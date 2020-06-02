9a01ecb63c87c0374bfa89eede78da8a0d5e6b17
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-02 16:59:08 -0700

    Add files via upload

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="kanji_app_db"
+)
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,13 @@
-from application import app
+from application import app, mydb
 from flask import render_template
+
+mycursor = mydb.cursor()
 
 @app.route("/")
 @app.route("/index")
 def index():
+    mycursor.execute("SELECT * FROM kanji_dict")
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
     return render_template("index.html", nav_index="active")
