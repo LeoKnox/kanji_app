881c9df1e61893775cb772c1243b7d994b802db2
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-02 18:34:41 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -2,10 +2,16 @@ from application import app, mydb
 from flask import render_template
 
 mycursor = mydb.cursor()
+mc = mydb.cursor()
 
 @app.route("/")
 @app.route("/index")
 def index():
     mycursor.execute("SELECT * FROM kanji_dict")
+    mycursor.fetchall()
+    kanjinumber = mycursor.rowcount
+    mycursor.execute("SELECT DISTINCT grade FROM kanji_dict")
     myresult = mycursor.fetchall()
-    return render_template("index.html", nav_index="active", myresult=myresult)
+    print('-->', end=": ")
+    print(kanjinumber)
+    return render_template("index.html", nav_index="active", myresult=myresult, kanjinumber=kanjinumber)
