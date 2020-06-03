ce35106a06d60e8f66ad74a712a10045c44fb055
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-03 14:54:45 -0700

    Add files via upload

--- application/static/js/practice.js
+++ application/static/js/practice.js
@@ -7,3 +7,30 @@ function nextKanji(x) {
         <td>"+x[y][5]+"</td>\
         <td>"+x[y][6]+"</td>";
 }
+
+function drawStroke(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    var coor = "Coordinates: (" + x + "," + y + ")";
+    document.addEventListener("mousemove", mm);
+    //document.getElementById("")
+}
+
+function mm(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    var svg = document.getElementById('draw');
+    var point = svg.createSVGPoint();
+    drawStroke(e);
+    //document.getElementById("")
+    point.x +=(x);
+    point.y +=(y);
+    var polyline = document.getElementById('polypoint');
+    polyline.points.appendItem(point);
+}
+
+function clearCoor() {
+    document.getElementById('polypoint').id="";
+    document.getElementById('draw').innerHTML += '<polyline id="polypoint" points="" style="fill:none;stroke:#000000;stroke-width:6" />';
+    document.removeEventListener('mousemove', mm)
+}
