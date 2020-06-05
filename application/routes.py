dce2858b8e754a8a4fff25e15b05ab033ea378ed
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-05 16:33:59 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -1,18 +1,20 @@
 from application import app, mydb
-from flask import render_template
+from flask import render_template, request
 import json, random, math
 
 mycursor = mydb.cursor()
 mc = mydb.cursor()
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index", methods=["GET", "POST"])
 def index():
     mycursor.execute("SELECT * FROM kanji_dict")
     mycursor.fetchall()
     kanjinumber = mycursor.rowcount
     mycursor.execute("SELECT DISTINCT grade FROM kanji_dict")
     myresult = mycursor.fetchall()
+    if request.method == "POST":
+        print("------> posted")
     return render_template("index.html", nav_index="active", myresult=myresult, kanjinumber=kanjinumber)
 
 @app.route("/practice")
