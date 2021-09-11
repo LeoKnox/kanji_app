aa282e1f9db561bf901f143c0b40f4870368cbac
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-10 20:11:38 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -34,6 +34,7 @@ def practice():
 
 @app.route("/practice", methods=["POST"])
 def add_kanji_to_db():
+    print(session['kanji_number'])
     print(request.form["kanji_number"])
     return redirect("/my_kanji")
 
