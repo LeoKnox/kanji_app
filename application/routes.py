82108b9fd8ca42831f6fe492ac83a44bf8807181
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-12 19:21:23 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -70,11 +70,11 @@ def remember_kanji():
     print("********")
     print(form.remember_kanji)
     mysql = connectToMySQL("first_flask")
-    query = "INSERT INTO my_kanji (kanji_dict_id) VALUES (%(mk)i;"
+    query = "INSERT INTO my_kanji (kanji_dict_id) VALUES (%(mk)i);"
     data = {
-        "mk": request.form["my_kanji_id"}]
+        "mk": request.form["my_kanji_id"]
     }
-    my_kanji = mysql.querty_db(query, data)
+    my_kanji = mysql.query_db(query, data)
     return render_template("about.html", nav_about="active")
 
 @app.route("/my_kanji")
