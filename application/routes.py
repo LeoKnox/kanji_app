934f2218583cbaa8a6f63acf2636815042de643d
LeoKnox <LeoKnox@users.noreply.github.com>
2021-09-12 19:52:57 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -65,14 +65,14 @@ def quiz(timer=7):
 def about():
     return render_template("about.html", nav_about="active")
 
-@app.route("/remember_kanji")
+@app.route("/remember_kanji", methods=["POST"])
 def remember_kanji():
     print("********")
-    print(form.remember_kanji)
+    print(request.form["remember_kanji"])
     mysql = connectToMySQL("first_flask")
     query = "INSERT INTO my_kanji (kanji_dict_id) VALUES (%(mk)d);"
     data = {
-        "mk": request.form.remember_kanji
+        "mk": request.form["remember_kanji"]
     }
     my_kanji = mysql.query_db(query, data)
     return render_template("about.html", nav_about="active")
