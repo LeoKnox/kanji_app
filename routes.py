1b25beaed419f49f660009b249a6bfcd11d64e74
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-18 12:41:53 -0700

    Add files via upload

--- routes.py
+++ routes.py
@@ -75,7 +75,7 @@ def remember_kanji():
     kdata=request.form["kanji_number"]
     print("********")
     #mk = ("INSERT INTO my_kanji (kanji_dict_id) VALUES (%d)", (data))
-    mycursor.execute("INSERT INTO kanji_app_db.my_kanji(kanji_dict_id) VALUES (%d)", (kdata))
+    mycursor.execute("INSERT INTO kanji_app_db.my_kanji(kanji_dict_id) VALUES (%d)", kdata)
     mydb.commit()
     #mysql = connectToMySQL("first_flask")
     #query = "INSERT INTO my_kanji (kanji_dict_id) VALUES (%(mk)d);"
