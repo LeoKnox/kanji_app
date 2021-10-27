41d63a30f9068d6437214bf24816b8b74d1596a2
LeoKnox <LeoKnox@users.noreply.github.com>
2021-10-27 12:35:35 -0700

    Add files via upload

--- static/js/practice.js
+++ static/js/practice.js
@@ -1,12 +1,12 @@
 async function nextKanji(x, y) {
-    y++;
-    document.geteElementById("test_practice").value = x;
+    y += 1;
     if (await y == x.length) {
         y = 0;
     };
     document.getElementById("next_button").value = y;
     document.getElementById("kanji_number").value = x[y][0];
     document.getElementById("display").innerHTML = x[y][1];
+    document.getElementById("test_practice").innerHTML = x[y][0];
     document.getElementById("kanji_row").innerHTML = "\
         <td>"+x[y][1]+"</td>\
         <td>"+x[y][3]+"</td>\
